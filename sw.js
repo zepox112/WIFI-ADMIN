a88860173c300e14ec91a3a70f0b0be4021999cd
Zepox OG <zepoxkenny4@gmail.com>
2026-06-10 17:06:55 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'zepox-cache-v1';
+const assets = [
+    '/',
+    '/index.html',
+    '/logo.png'
+];
+
+// Sakinisha Service Worker (Cache files)
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(assets);
+        })
+    );
+});
+
+// Fanya kazi hata offline
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
